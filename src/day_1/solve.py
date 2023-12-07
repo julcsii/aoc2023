ce39ae94dd83b201709bed6ba9199619592e6862
Julia Hermann <julia.hermann@magenta.at>
2023-12-07 12:22:04 +0100

    Refactor with chatgpt

--- src/day_1/solve.py
+++ src/day_1/solve.py
@@ -1,4 +1,3 @@
-
 import logging
 import sys
 from typing import List, Optional
@@ -8,9 +7,7 @@ import regex as re
 
 from src.utils.io import read_txt
 
-
-cfg = OmegaConf.load("config.yaml") 
-
+cfg = OmegaConf.load("config.yaml")
 
 logging.basicConfig(
     stream=sys.stdout,
@@ -21,37 +18,29 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def extract_calibration_codes(inputs: List[str]) -> List[int]:
-    calibration_codes = []
-    for i in inputs:
-        code = extract_calibration_code(i)
-        if code:
-            calibration_codes.append(code)
-    return calibration_codes    
+    return [code for i in inputs if (code := extract_calibration_code(i)) is not None]
 
-
-def extract_calibration_code(i: str) -> Optional[int]:
-    "Returns the first and last digit from a list of strings"
+def extract_calibration_code(input_str: str) -> Optional[int]:
     digits_extracted = []
     text_chars = ""
-    for ch in i:
+
+    for ch in input_str:
         if not ch.isalnum():
             text_chars = ""
         if ch.isnumeric():
-            digits = extract_text_digits(text_chars)
-            digits_extracted += digits
+            digits_extracted.extend(extract_text_digits(text_chars))
             digits_extracted.append(ch)
             text_chars = ""
         if ch.isalpha():
             text_chars += ch
-    digits = extract_text_digits(text_chars)
-    digits_extracted += digits
-    if digits_extracted and len(digits_extracted) > 0:
+
+    digits_extracted.extend(extract_text_digits(text_chars))
+
+    if digits_extracted:
         return int(f"{digits_extracted[0]}{digits_extracted[-1]}")
     return None
 
-
 def extract_text_digits(input_string: str) -> List[int]:
-    # Define a dictionary to map text representations to their numeric equivalents
     text_to_digit_mapping = {
         "one": "1",
         "two": "2",
@@ -66,25 +55,17 @@ def extract_text_digits(input_string: str) -> List[int]:
         "null": "0"
     }
 
-    # Create a regex pattern to match any of the text representations
     pattern = re.compile('|'.join(map(re.escape, text_to_digit_mapping.keys())), re.IGNORECASE)
-
-    # Find all matches in the input string
     matches = re.findall(pattern, input_string, overlapped=True)
 
-    # Convert text representations to numeric values
-    result = [int(text_to_digit_mapping[match.lower()]) for match in matches]
-
-    return result
+    return [int(text_to_digit_mapping[match.lower()]) for match in matches]
 
 rows_to_print = 10
-
 input_list = read_txt(cfg.data.input)
 logger.debug(input_list[:rows_to_print])
 
 calibration_codes = extract_calibration_codes(input_list)
 logger.debug(calibration_codes[:rows_to_print])
 
-sum = sum(calibration_codes)
-
-logger.info(sum)
+total_sum = sum(calibration_codes)
+logger.info(total_sum)
